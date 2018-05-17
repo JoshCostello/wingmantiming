5928a660b27379f42e9ac341c43680160b05830d
Joshua Costello <costeljm@gmail.com>
2018-05-16 21:05:20 -0400

    feat: adds google analytics

--- wp-content/themes/wingman/header.php
+++ wp-content/themes/wingman/header.php
@@ -4,11 +4,23 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
+
+    <?php if(defined('GOOGLE_ANALYTICS_API_KEY')): ?>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo GOOGLE_ANALYTICS_API_KEY ?>"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '<?php echo GOOGLE_ANALYTICS_API_KEY; ?>');
+        </script>
+    <?php endif; ?>
+
 </head>
 
 <body <?php body_class(); ?>>
     <a class="visually-hidden" href="#content"><?php _e( 'Skip to content', 'wingman' ); ?></a>
-
     <header class="site__header">
         <div class="site__header-inner">
             <a class="brand brand__link" href="<?php bloginfo('url') ?>">
