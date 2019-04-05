946e712c971652a53163f8fc554693af1d836b19
Joshua Costello <costeljm@gmail.com>
2019-04-04 22:53:46 -0400

    feat: adds functionality to support using a color picker for meet overlay colors

--- wp-content/themes/wingman/single-meet.php
+++ wp-content/themes/wingman/single-meet.php
@@ -2,32 +2,66 @@
 //meet layout
 get_header();
 
-    if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+    <?php $overlay_color = get_field('overlay_color'); ?>
+    <?php if (get_field('overlay_color_type') === "picker"): ?>
+        <?php
+            $raw_color = get_field('overlay_color_picker');
+            $overlay_color = str_replace("#", "", $raw_color);
+            
+        ?>
+        <style>
+            .meet__name-wrapper--<?php echo $overlay_color; ?>::after {
+                background: <?php echo $raw_color; ?>;
+            }
+
+            .meet__body--<?php echo $overlay_color; ?> {
+                border-color: <?php echo $raw_color; ?>;
+            }
+
+            .meet__details--<?php echo $overlay_color; ?> {
+                border: 10px solid <?php echo $raw_color; ?>;
+            }
+
+            .meet__details-inner--<?php echo $overlay_color; ?> {
+                border-color: <?php echo $raw_color; ?>;
+            }
+
+            .meet__link--<?php echo $overlay_color; ?> {
+                box-shadow: inset 0px -8px 0px 0px <?php echo hex2rgba($raw_color, 0.75); ?>;
+            }
+
+            .meet__link--<?php echo $overlay_color; ?>:hover {
+                box-shadow: inset 0px -12px 0px 0px <?php echo hex2rgba($raw_color, 0.75); ?>;
+            }
+        </style>
+    <?php endif; ?>
 
     <header class="meet__header">
         <div class="meet__banner-background" style="background-image: url('<?php echo get_field('banner_background')['url']; ?>');"></div>
-        <div class="meet__name-wrapper meet__name-wrapper--<?php echo get_field('overlay_color'); ?>">
+        <div class="meet__name-wrapper meet__name-wrapper--<?php echo $overlay_color; ?>">
             <h1 class="meet__name"><?php the_title(); ?></h1>
         </div>
     </header>
 
-    <div class="meet__body meet__body--<?php echo(get_field('logo')['url'] ? 'has-logo' : 'no-logo'); ?> meet__body--<?php echo get_field('overlay_color'); ?>">
+    <div class="meet__body meet__body--<?php echo(get_field('logo')['url'] ? 'has-logo' : 'no-logo'); ?> meet__body--<?php echo $overlay_color; ?>">
         <div class="meet__content meet__content--<?php echo(get_field('logo')['url'] ? 'has-logo' : 'no-logo'); ?>">
-            <div class="meet__details meet__details--<?php echo get_field('overlay_color'); ?>">
+            <div class="meet__details meet__details--<?php echo $overlay_color; ?>">
                 <div class="meet__logo">
-                    <?php if(get_field('logo')['url']): ?>
+                    <?php if (get_field('logo')['url']): ?>
                         <img class="meet__logo-image" src="<?php echo get_field('logo')['url']; ?>" />
                     <?php endif; ?>
                 </div>
 
-                <div class="meet__details-inner meet__details-inner--<?php echo get_field('overlay_color'); ?>">
+                <div class="meet__details-inner meet__details-inner--<?php echo $overlay_color; ?>">
                     <p>
                         <small class="meet__details--small">Dates</small>
                         <?php
                             $start_date = get_field('start_date');
                             $end_date = get_field('end_date');
                         ?>
-                        <?php if($start_date == $end_date): ?>
+                        <?php if ($start_date == $end_date): ?>
                             <?php echo get_field('start_date'); ?>
                         <?php else: ?>
                             <?php echo get_field('start_date'); ?> &ndash; <?php echo get_field('end_date'); ?>
@@ -38,29 +72,29 @@ get_header();
                     </p>
 
                     <ul class="meet__links-list">
-                        <?php if(get_field('results')): ?>
+                        <?php if (get_field('results')): ?>
                             <li class="meet__links-list-item">
-                                <a href="<?php echo get_field('results'); ?>" class="meet__link meet__link--<?php echo get_field('overlay_color'); ?>">Live Results</a>
+                                <a href="<?php echo get_field('results'); ?>" class="meet__link meet__link--<?php echo $overlay_color; ?>">Live Results</a>
                             </li>
                         <?php endif; ?>
-                        <?php if(get_field('final_results')): ?>
+                        <?php if (get_field('final_results')): ?>
                             <li class="meet__links-list-item">
-                                <a href="<?php echo get_field('final_results')['url']; ?>" class="meet__link meet__link--<?php echo get_field('overlay_color'); ?>">Final Results</a>
+                                <a href="<?php echo get_field('final_results')['url']; ?>" class="meet__link meet__link--<?php echo $overlay_color; ?>">Final Results</a>
                             </li>
                         <?php endif; ?>
-                        <?php if(get_field('meet_program')): ?>
+                        <?php if (get_field('meet_program')): ?>
                             <li class="meet__links-list-item">
-                                <a href="<?php echo get_field('meet_program')['url']; ?>" class="meet__link meet__link--<?php echo get_field('overlay_color'); ?>">Meet Program</a>
+                                <a href="<?php echo get_field('meet_program')['url']; ?>" class="meet__link meet__link--<?php echo $overlay_color; ?>">Meet Program</a>
                             </li>
                         <?php endif; ?>
-                        <?php if(get_field('meet_details')): ?>
+                        <?php if (get_field('meet_details')): ?>
                             <li class="meet__links-list-item">
-                                <a href="<?php echo get_field('meet_details')['url']; ?>" class="meet__link meet__link--<?php echo get_field('overlay_color'); ?>">Meet Details</a>
+                                <a href="<?php echo get_field('meet_details')['url']; ?>" class="meet__link meet__link--<?php echo $overlay_color; ?>">Meet Details</a>
                             </li>
                         <?php endif; ?>
-                        <?php if(get_field('website')): ?>
+                        <?php if (get_field('website')): ?>
                             <li class="meet__links-list-item">
-                                <a href="<?php echo get_field('website'); ?>" class="meet__link meet__link--<?php echo get_field('overlay_color'); ?>">Meet Website</a>
+                                <a href="<?php echo get_field('website'); ?>" class="meet__link meet__link--<?php echo $overlay_color; ?>">Meet Website</a>
                             </li>
                         <?php endif; ?>
                     </ul>
@@ -73,9 +107,9 @@ get_header();
     </div>
 
     <?php
-        if(have_rows('components')) :
+        if (have_rows('components')) :
             $block_count = 2;
-            while(have_rows('components')) : the_row();
+            while (have_rows('components')) : the_row();
                 ACF_Layout::render(get_row_layout(), $block_count);
                 $block_count++;
             endwhile;
@@ -101,6 +135,6 @@ get_header();
 </script>
 <script src="https://maps.googleapis.com/maps/api/js?key=<?php echo GOOGLE_MAPS_API_KEY; ?>&callback=initMap" async defer></script>
 
-<?php 
+<?php
     get_footer();
 ?>
