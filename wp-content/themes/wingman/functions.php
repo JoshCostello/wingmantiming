ae0d98dcf633bd5a2e25bf0d8f585b4119cf168a
Joshua Costello <costeljm@gmail.com>
2019-04-05 23:07:52 -0400

    feat: add sorting of meets in the dash by meet start date

--- wp-content/themes/wingman/functions.php
+++ wp-content/themes/wingman/functions.php
@@ -16,10 +16,63 @@ $theme_includes = [
 
 
 foreach ($theme_includes as $file) {
-  if (!$filepath = locate_template($file)) {
-    trigger_error(sprintf(__('Error locating %s for inclusion'), $file), E_USER_ERROR);
-  }
+    if (!$filepath = locate_template($file)) {
+        trigger_error(sprintf(__('Error locating %s for inclusion'), $file), E_USER_ERROR);
+    }
 
-  require_once $filepath;
+    require_once $filepath;
 }
 unset($file, $filepath);
+
+
+add_filter('manage_meet_posts_columns', 'set_custom_edit_meet_columns');
+
+function set_custom_edit_meet_columns($columns)
+{
+    $date = $columns['date'];
+    unset($columns['date']);
+
+    $columns['meet_date'] = __('Meet Date', 'wingman-meet-date');
+
+    $columns['date'] = $date;
+
+    return $columns;
+}
+
+add_action('manage_meet_posts_custom_column', 'custom_meet_column', 10, 2);
+
+function custom_meet_column($column, $post_id)
+{
+    switch ($column) {
+    // display the value of an ACF (Advanced Custom Fields) field
+    case 'meet_date':
+      echo get_field('start_date', $post_id);
+      break;
+
+  }
+}
+
+add_filter('manage_edit-meet_sortable_columns', 'set_custom_meet_sortable_columns');
+
+function set_custom_meet_sortable_columns($columns)
+{
+    $columns['meet_date'] = 'meet_date';
+
+    return $columns;
+}
+
+add_action('pre_get_posts', 'meet_custom_orderby');
+
+function meet_custom_orderby($query)
+{
+    if (! is_admin()) {
+        return;
+    }
+
+    $orderby = $query->get('orderby');
+
+    if ('start_date' == $orderby) {
+        $query->set('meta_key', 'start_date');
+        $query->set('orderby', 'meta_value');
+    }
+}
